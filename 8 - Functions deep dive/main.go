e0577fdfe3b34caf5df9e854a70b568bf0c3a11f
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-02 16:46:29 -0600

    chore: add transformNumbersWithNumber

--- 8 - Functions deep dive/main.go
+++ 8 - Functions deep dive/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	Double = 2
+	Triple = 3
+)
+
 //this is creating a new type that would be the same as int | float32 | float64 in TS if those types existed
 type Numeric interface {
 	~float32 |
@@ -19,16 +24,44 @@ func main() {
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbersFloat, triple)
 
-	fmt.Printf("Doubled ints: %v\n", doubled)
-	fmt.Printf("Triple float64: %v\n", tripled)
+	fmt.Printf("Doubled ints using transformNumbers: %v\n", doubled)
+	fmt.Printf("Triple float64 using transformNumbers: %v\n", tripled)
+
+	doubled = transformNumbersWithNumber(&numbers, 2)
+	tripled = transformNumbersWithNumber(&numbersFloat, 3)
+
+	fmt.Printf("Doubled ints using transformNumbersWithNumber: %v\n", doubled)
+	fmt.Printf("Triple float64 using transformNumbersWithNumber: %v\n", tripled)
 }
 
 func transformNumbers[T Numeric](numbers *[]T, transformer transformerFn[T]) []T {
-	doubleNumbers := []T{}
+	newNumbers := []T{}
 	for _, value := range *numbers {
-		doubleNumbers = append(doubleNumbers, transformer(value))
+		newNumbers = append(newNumbers, transformer(value))
 	}
-	return doubleNumbers
+	return newNumbers
+}
+
+func transformNumbersWithNumber[T Numeric](numbers *[]T, transformerType int) []T {
+	newNumbers := []T{}
+	for _, value := range *numbers {
+		newNumbers = append(newNumbers, getTransformerFn[T](transformerType)(value))
+	}
+	return newNumbers
+}
+
+func getTransformerFn[T Numeric](transformerType ...int) transformerFn[T] {
+	if len(transformerType) == 0 {
+		return double
+	}
+	switch transformerType[0] {
+		case Double:
+			return double
+		case Triple:
+			return triple
+		default:
+			return double
+	}
 }
 
 func double[T Numeric](number T) T {
