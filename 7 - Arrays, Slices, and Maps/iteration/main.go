86d614613a2bb25603874a36f980672d2892f36a
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-02 15:17:55 -0600

    add iteration examples

---
+++ 7 - Arrays, Slices, and Maps/iteration/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	LinkedIn = "linkedIn"
+	Aws = "aws"
+	Google = "google"
+)
+
+type WebsitesMapKey string
+type WebsitesMap map[WebsitesMapKey]string
+
+func main () {
+	websites := WebsitesMap{
+		LinkedIn: "https://google.com",
+		Aws: "https://aws.com",
+	}
+
+	userNames := make([]string, 2, 10)
+	userNames = append(userNames, "Adam")
+	userNames = append(userNames, "Test")
+
+	for range userNames {
+		fmt.Println("The most basic type of iteration when you want to do something the same number of times as a map or array but don't need to use the key or value")
+	}
+
+	//this is just iteration over the keys
+	for index := range userNames {
+		fmt.Printf("userName's index is '%v'\n", index)
+	}
+
+	//this is iteration when you want key and value
+	for index, value:= range userNames {
+		fmt.Printf("userName's index '%d' is '%v'\n", index, value)
+	}
+
+	//the same syntax for iteratin maps
+	for index, value := range websites {
+		fmt.Printf("website's index '%v' is '%v'\n", index, value)
+	}
+}
